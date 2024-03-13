0ac60ee6ee5cd5486207dec8265cc75a1202a035
ALEXANDER <msumba83@gmail.com>
2024-03-13 20:28:35 +0200

    done variables

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+// .......JAVASCRIPT IN THE CONSOLE.......
 // console.log(`Hello`);
 // console.log(`I like pizza`)
 
 // window.alert(`This is an alert`);
 // window.alert(`I like pizza`)
+
+// .....TO GET HTML ELEMENT .....
+/*document.getElementById('my-h1').textContent = 'Capricorn C.R.M.I';
+document.getElementById('my-p').textContent = `I like pizza`
+*/
+
+// ..... VARIABLES .......
+// 1. declaration: let x;
+// 2. assignment: x = 10;
+
+let x = 79; //declaration and assignment
+console.log(x);
+
+// .... DATA TYPES
+
+// 1 NUMBERS 
+let age = 39;
+let price = 24.56;
+let gpa = 2.1;
+
+// 2 STRINGS 
+let firstName = "Alexander";
+console.log(typeof firstName)
