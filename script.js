1f4ba5ed4df295bd1c6b71573cd8d3c10da43c55
ALEXANDER <msumba83@gmail.com>
2024-03-14 21:12:31 +0200

    done operators

--- script.js
+++ script.js
@@ -14,16 +14,67 @@ document.getElementById('my-p').textContent = `I like pizza`
 // 1. declaration: let x;
 // 2. assignment: x = 10;
 
-let x = 79; //declaration and assignment
-console.log(x);
+// let x = 79; //declaration and assignment
+//console.log(x);
 
 // .... DATA TYPES
 
-// 1 NUMBERS 
-let age = 39;
-let price = 24.56;
-let gpa = 2.1;
+// 1 NUMBERS
+// let age = 39;
+// let price = 24.56;
+// let gpa = 2.1;
 
-// 2 STRINGS 
-let firstName = "Alexander";
-console.log(typeof firstName)
+// 2 STRINGS
+/*let firstName = "Alexander";
+let favoriteFood = "Fish"
+
+console.log(typeof firstName);
+console.log(`my favorite food is ${favoriteFood}`);
+console.log(`Your name is ${firstName}`); */
+
+// 3 BOOLEAN
+// let online = false;
+
+// console.log(`Alex is online ${online}`);
+// console.log(typeof online);
+
+/*let fullName = 'Alexander Msumba';
+let age = 34;
+let isStudent = false;
+
+document.getElementById('p1').textContent = `My name is ${fullName}`;
+document.getElementById('p2').textContent = `I am ${age} years old`;
+document.getElementById('p3').textContent = `Enrolled: ${isStudent}`; */
+
+// ====== OPERATORS ======
+
+let myStudents = 30;
+// myStudents = myStudents + 2; // addition
+// myStudents = myStudents - 2;     // subtraction
+// myStudents = myStudents / 2 divide
+// myStudents = myStudents * 10 multiplication
+// myStudents = myStudents ** 2; exponents;
+// let  ourStudents = myStudents % 2 //modula
+// myStudents += 5;
+// myStudents -= 3
+// myStudents *= 2
+// myStudents /= 2;
+// myStudents **= 2;
+// myStudents %= 2
+
+// ===== INCREMENT OPERATOR ====
+// myStudents++ this is plus 1
+// myStudents--  this is minus 1 
+
+console.log(myStudents);
+
+/*operator precedence 
+parenthesis ()
+exponents
+multiplication & division & modulo
+addition & subtraction
+*/
+// let result = 1 + 2 * 3 + 4 ** 2
+let result = 12 % 5 + 8 / 2;
+
+console.log(result)
