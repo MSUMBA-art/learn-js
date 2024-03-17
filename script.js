b66149f2a0a8b6c8d86a50541d13f1c777000ec3
ALEXANDER <msumba83@gmail.com>
2024-03-17 08:50:40 +0200

    done Math() method

--- script.js
+++ script.js
@@ -121,15 +121,54 @@ type conversion = change the datatype of a value to another (string, number, boo
 // console.log(y, typeof y);
 // console.log(z, typeof z);
 
-// ===== CONST = a variable that cant be changed 
-const PI = 3.34159;
-let radius;
-let circumference;
+// ===== CONST = a variable that cant be changed
+// const PI = 3.34159;
+// let radius;
+// let circumference;
 
+// document.getElementById("mySubmit").onclick = function () {
+//     radius = document.getElementById("myText").value;
+//     radius = Number(radius);
+//     circumference = 2 * PI * radius;
+//     document.getElementById("myH3").textContent = circumference + "cm";
+// }
 
-document.getElementById("mySubmit").onclick = function () {
-    radius = document.getElementById("myText").value;
-    radius = Number(radius);
-    circumference = 2 * PI * radius;
-    document.getElementById("myH3").textContent = circumference + "cm";
-}
+// ======== COUNT TIMER =======
+
+/*const decreaseBtn = document.getElementById('decreaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const countLabel = document.getElementById('countLabel');
+let count = 0;
+
+increaseBtn.onclick = function () {
+  count++;
+  countLabel.textContent = count;
+};
+
+decreaseBtn.onclick = function () {
+  count--;
+  countLabel.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  countLabel.textContent = count;
+};
+*/
+
+// ======= MATH METHOD 
+let x = 10;
+let y = 2;
+let z = 9;
+
+// z = Math.round(x);
+// z = Math.floor(x); floor, trunc and round do the same thing
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(y, x);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// let max = Math.max(x, y, z)
+// let min = Math.min(x, y, z);
+// console.log(min);
