c013b4e221570f01220510463f172ed72c53361c
ALEXANDER <msumba83@gmail.com>
2024-04-03 06:27:02 +0200

    done checked property

--- script.js
+++ script.js
@@ -223,7 +223,7 @@ myBtn.onclick = function () {
 
 //
 
-const myText = document.getElementById('myText');
+/*const myText = document.getElementById('myText');
 const mySubmit = document.getElementById('mySubmit');
 const resultElement = document.getElementById('resultElement');
 let age;
@@ -243,3 +243,35 @@ mySubmit.onclick = function () {
     resultElement.textContent = 'You must be 18+ to enter this site';
   }
 };
+*/
+
+// ===== CHECKED BOX PROPERTY =======
+const myCheckbox = document.getElementById('myCheckbox');
+const visaBtn = document.getElementById('visaBtn');
+const mastercardBtn = document.getElementById('mastercardBtn');
+const payPalBtn = document.getElementById('payPalBtn');
+const mySubmit = document.getElementById('mySubmit');
+const subResult = document.getElementById('subResult');
+const paymentResult = document.getElementById('paymentResult');
+
+mySubmit.onclick = function () {
+  if (myCheckbox.checked) {
+    subResult.textContent = `You are subscribed`;
+  } 
+  else {
+     subResult.textContent = `You are NOT subscribed`;
+  }
+
+  if (visaBtn.checked) {
+    paymentResult.textContent = `You are paying with Visa`
+  }
+  else if (mastercardBtn.checked) {
+    paymentResult.textContent = `You are paying with MasterCard`
+  }
+  else if (payPalBtn.checked) {
+    paymentResult.textContent = `You are paying with PayPal`
+  }
+  else {
+    paymentResult.textContent = `You must select a payment type`
+  }
+}
